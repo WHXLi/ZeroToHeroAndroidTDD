fe4186628f7898ce95169f6c3f7a5d6fd63f6570
v.menshov <v.menshov@axelot.ru>
2025-11-14 15:56:17 +0300

    Task009Test passed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -4,16 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 
 private const val TITLE_TEXT = "title_text"
 private const val TITLE_VISIBILITY = "title_visibility"
+private const val TITLE_IS_ATTACHED = "title_is_attached"
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var rootView: LinearLayout
     private lateinit var titleTextView: TextView
     private lateinit var changeBtn: Button
     private lateinit var hideBtn: Button
+    private lateinit var removeBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,24 +30,34 @@ class MainActivity : AppCompatActivity() {
         super.onSaveInstanceState(outState)
         outState.putString(TITLE_TEXT, titleTextView.text.toString())
         outState.putInt(TITLE_VISIBILITY, titleTextView.visibility)
+        outState.putBoolean(TITLE_IS_ATTACHED, titleTextView.isAttachedToWindow)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        savedInstanceState.let {
-            titleTextView.text = it.getString(TITLE_TEXT)
-            titleTextView.visibility = it.getInt(TITLE_VISIBILITY)
-        }
+        restoreTitleTextView(savedInstanceState)
     }
 
     private fun initViews() {
+        rootView = findViewById(R.id.rootLayout)
         titleTextView = findViewById(R.id.titleTextView)
         changeBtn = findViewById(R.id.changeButton)
         hideBtn = findViewById(R.id.hideButton)
+        removeBtn = findViewById(R.id.removeButton)
     }
 
     private fun setListeners() {
         changeBtn.setOnClickListener { titleTextView.text = getString(R.string.android_dev) }
         hideBtn.setOnClickListener { titleTextView.visibility = View.GONE }
+        removeBtn.setOnClickListener { rootView.removeView(titleTextView) }
+    }
+
+    private fun restoreTitleTextView(savedInstanceState: Bundle) {
+        val notAttached = !savedInstanceState.getBoolean(TITLE_IS_ATTACHED)
+        if (notAttached) rootView.removeView(titleTextView)
+        else {
+            titleTextView.text = savedInstanceState.getString(TITLE_TEXT)
+            titleTextView.visibility = savedInstanceState.getInt(TITLE_VISIBILITY)
+        }
     }
 }
