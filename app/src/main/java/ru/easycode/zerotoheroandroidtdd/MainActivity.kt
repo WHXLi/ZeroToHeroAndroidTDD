1785aac6f5ac263d853919afa6f5530a64ec379c
v.menshov <v.menshov@axelot.ru>
2025-11-14 11:24:13 +0300

    Task007Test passed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,30 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.TextView
+
+private const val TITLE = "title"
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var titleTextView: TextView
+    private lateinit var changeBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        titleTextView = findViewById(R.id.titleTextView)
+        changeBtn = findViewById(R.id.changeButton)
+        savedInstanceState?.let {
+            titleTextView.text = it.getString(TITLE)
+        }
+        changeBtn.setOnClickListener { titleTextView.text = getString(R.string.android_dev) }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(TITLE, titleTextView.text.toString())
     }
 }
