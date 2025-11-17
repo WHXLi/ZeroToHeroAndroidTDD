6d17ad652591e3ea68bca512f2b327e53b013884
v.menshov <v.menshov@axelot.ru>
2025-11-17 17:33:57 +0300

    Task011Test OOP passed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -8,17 +8,22 @@ import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.view.isVisible
+import ru.easycode.zerotoheroandroidtdd.count.Count
 
-private const val TEXT_VIEW_STATE_MANAGER = "TEXT_VIEW_STATE_MANAGER"
+private const val TITLE_MANAGER = "TITLE_MANAGER"
+private const val COUNT_MANAGER = "COUNT_MANAGER"
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textViewStateManager: TextViewStateManager
+    private lateinit var titleTextViewManager: TextViewStateManager
+    private lateinit var countTextViewManager: TextViewStateManager
     private lateinit var rootView: LinearLayout
     private lateinit var titleTextView: TextView
-    private lateinit var changeBtn: Button
-    private lateinit var hideBtn: Button
-    private lateinit var removeBtn: Button
+    private lateinit var changeButton: Button
+    private lateinit var hideButton: Button
+    private lateinit var removeButton: Button
+    private lateinit var countTextView: TextView
+    private lateinit var incrementButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,53 +34,71 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putSerializable(TEXT_VIEW_STATE_MANAGER, textViewStateManager)
+        outState.putSerializable(TITLE_MANAGER, titleTextViewManager)
+        outState.putSerializable(COUNT_MANAGER, countTextViewManager)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        savedInstanceState.restoreTextViewManager()?.let { this.textViewStateManager = it }
+        savedInstanceState.restoreTextViewManager(TITLE_MANAGER)?.let {
+            this.titleTextViewManager = it
+        }
+        savedInstanceState.restoreTextViewManager(COUNT_MANAGER)?.let {
+            this.countTextViewManager = it
+        }
     }
 
     private fun initViews() {
         rootView = findViewById(R.id.rootLayout)
-        titleTextView = findViewById(R.id.titleTextView)
-        changeBtn = findViewById(R.id.changeButton)
-        hideBtn = findViewById(R.id.hideButton)
-        removeBtn = findViewById(R.id.removeButton)
-        textViewStateManager = TextViewStateManager(
-            textView = titleTextView,
-            hideButton = hideBtn,
-            removeButton = removeBtn,
-        )
+        countTextView = findViewById(R.id.countTextView)
+        changeButton = findViewById(R.id.changeButton)
+        hideButton = findViewById(R.id.hideButton)
+        removeButton = findViewById(R.id.removeButton)
+        incrementButton = findViewById(R.id.incrementButton)
+        titleTextView = findViewById<TextView>(R.id.titleTextView).apply {
+            titleTextViewManager = TextViewStateManager(
+                textView = this,
+                removeButton = removeButton,
+                hideButton = hideButton,
+            )
+        }
+        countTextView = findViewById<TextView>(R.id.countTextView).apply {
+            countTextViewManager = TextViewStateManager(this)
+        }
     }
 
     private fun setListeners() {
-        changeBtn.setOnClickListener {
-            textViewStateManager.changeText(getString(R.string.android_dev))
+        changeButton.setOnClickListener {
+            titleTextViewManager.changeText(getString(R.string.android_dev))
         }
-        hideBtn.setOnClickListener {
-            if (titleTextView.isVisible) textViewStateManager.hideView()
-            else textViewStateManager.show()
+        hideButton.setOnClickListener {
+            if (titleTextView.isVisible) titleTextViewManager.hideView()
+            else titleTextViewManager.show()
         }
-        removeBtn.setOnClickListener {
-            textViewStateManager.removeView()
+        removeButton.setOnClickListener {
+            titleTextViewManager.removeView()
         }
-    }
-
-    private fun Bundle.restoreTextViewManager(): TextViewStateManager? {
-        return this.getTextViewManager()?.apply {
-            restore(
-                titleTextView,
-                removeBtn,
-                hideBtn,
+        incrementButton.setOnClickListener {
+            countTextViewManager.changeText(
+                Count.Base(2).increment(countTextView.text.toString())
             )
         }
     }
 
-    private fun Bundle.getTextViewManager(): TextViewStateManager? {
+    private fun Bundle.restoreTextViewManager(key: String, ) = this.getTextViewManager(key)?.apply {
+        when (key) {
+            TITLE_MANAGER -> restore(
+                textView = titleTextView,
+                removeButton = removeButton,
+                hideButton = hideButton,
+            )
+            COUNT_MANAGER -> restore(countTextView)
+        }
+    }
+
+    private fun Bundle.getTextViewManager(key: String): TextViewStateManager? {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            getSerializable(TEXT_VIEW_STATE_MANAGER, TextViewStateManager::class.java)
-        } else getSerializable(TEXT_VIEW_STATE_MANAGER) as? TextViewStateManager
+            getSerializable(key, TextViewStateManager::class.java)
+        } else getSerializable(key) as? TextViewStateManager
     }
 }
