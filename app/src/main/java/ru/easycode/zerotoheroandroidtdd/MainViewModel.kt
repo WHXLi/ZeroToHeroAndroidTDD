ab46c2afdf9f7836678ecf433452b41eb7ccc9ca
v.menshov <v.menshov@axelot.ru>
2025-11-18 16:18:06 +0300

    Task012Test OOP passed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainViewModel.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainViewModel.kt
@@ -1,7 +1,18 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import ru.easycode.zerotoheroandroidtdd.count.Count
+import ru.easycode.zerotoheroandroidtdd.state.UiState
 
-class MainViewModel: ViewModel() {
+class MainViewModel(): ViewModel() {
 
+    private val uiState = MutableLiveData<UiState>()
+
+    fun getUiState(): LiveData<UiState> = uiState
+
+    fun increment(number: String, step: Int, max: Int) {
+        uiState.postValue(Count.Base(step, max).increment(number))
+    }
 }
--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/count/Count.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/count/Count.kt
@@ -6,14 +6,27 @@ interface Count {
 
     fun increment(number: String): UiState
 
-    class Base(private val step: Int): Count {
+    class Base(
+        private val step: Int,
+        private val max: Int,
+    ): Count {
 
         init {
             if (step < 1 ) throw IllegalStateException(
                 "step should be positive, but was $step"
             )
+            if (max < 1) throw IllegalStateException(
+                "max should be positive, but was $max"
+            )
+            if (max < step) throw IllegalStateException(
+                "max should be more than step"
+            )
         }
 
-        override fun increment(number: String) = UiState()
+        override fun increment(number: String): UiState {
+            val incremented = number.toInt() + step
+            return if (incremented >= max) UiState.Max(max.toString())
+            else UiState.Base(incremented.toString())
+        }
     }
 }
